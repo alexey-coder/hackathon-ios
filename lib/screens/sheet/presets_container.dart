8f544745f47fba0cf6e25c066960d309268f65d0
Albert <alhafram@gmail.com>
2023-05-22 19:05:15 +0300

    Add search rows

--- lib/screens/sheet/presets_container.dart
+++ lib/screens/sheet/presets_container.dart
@@ -10,45 +10,43 @@ class PresetsContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var sheetProvider = context.read<SheetProvider>();
-    return Padding(
-        padding: const EdgeInsets.only(left: 24, right: 24),
-        child: Wrap(runSpacing: 8, children: [
-          Row(children: [
-            OrganizationTypeCell(
-                image: Assets.musicalSchools.image(),
-                onPressed: () =>
-                    sheetProvider.changeSearchText('Музыкальные школы')),
-            const Padding(padding: EdgeInsets.only(left: 8)),
-            OrganizationTypeCell(
-                image: Assets.artSchools.image(),
-                onPressed: () =>
-                    sheetProvider.changeSearchText('Художественные школы'))
-          ]),
-          Row(children: [
-            OrganizationTypeCell(
-                image: Assets.museums.image(),
-                onPressed: () => sheetProvider.changeSearchText('Музеи')),
-            const Padding(padding: EdgeInsets.only(left: 8)),
-            OrganizationTypeCell(
-                image: Assets.exhibitions.image(),
-                onPressed: () => sheetProvider.changeSearchText('Выставки')),
-            const Padding(padding: EdgeInsets.only(left: 8)),
-            OrganizationTypeCell(
-                image: Assets.theaters.image(),
-                onPressed: () => sheetProvider.changeSearchText('Театры'))
-          ]),
-          Row(children: [
-            FixedOrganizationTypeCell(
-                image: Assets.childrenArtSchools.image(),
-                width: 155,
-                onPressed: () =>
-                    sheetProvider.changeSearchText('Детские школы искусств')),
-            const Padding(padding: EdgeInsets.only(left: 8)),
-            OrganizationTypeCell(
-                image: Assets.danceSchools.image(),
-                onPressed: () =>
-                    sheetProvider.changeSearchText('Танцевальные школы'))
-          ])
-        ]));
+    return Wrap(runSpacing: 8, children: [
+      Row(children: [
+        OrganizationTypeCell(
+            image: Assets.musicalSchools.image(),
+            onPressed: () =>
+                sheetProvider.changeSearchText('Музыкальные школы')),
+        const Padding(padding: EdgeInsets.only(left: 8)),
+        OrganizationTypeCell(
+            image: Assets.artSchools.image(),
+            onPressed: () =>
+                sheetProvider.changeSearchText('Художественные школы'))
+      ]),
+      Row(children: [
+        OrganizationTypeCell(
+            image: Assets.museums.image(),
+            onPressed: () => sheetProvider.changeSearchText('Музеи')),
+        const Padding(padding: EdgeInsets.only(left: 8)),
+        OrganizationTypeCell(
+            image: Assets.exhibitions.image(),
+            onPressed: () => sheetProvider.changeSearchText('Выставки')),
+        const Padding(padding: EdgeInsets.only(left: 8)),
+        OrganizationTypeCell(
+            image: Assets.theaters.image(),
+            onPressed: () => sheetProvider.changeSearchText('Театры'))
+      ]),
+      Row(children: [
+        FixedOrganizationTypeCell(
+            image: Assets.childrenArtSchools.image(),
+            width: 155,
+            onPressed: () =>
+                sheetProvider.changeSearchText('Детские школы искусств')),
+        const Padding(padding: EdgeInsets.only(left: 8)),
+        OrganizationTypeCell(
+            image: Assets.danceSchools.image(),
+            onPressed: () =>
+                sheetProvider.changeSearchText('Танцевальные школы'))
+      ])
+    ]);
   }
 }
--- lib/screens/sheet/sheet_screen.dart
+++ lib/screens/sheet/sheet_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:maps/gen/assets.gen.dart';
+import 'package:maps/gen/fonts.gen.dart';
 import 'package:maps/providers/sheet_provider.dart';
 import 'package:maps/screens/sheet/presets_container.dart';
 import 'package:maps/screens/sheet/sheet_header.dart';
@@ -13,15 +15,17 @@ class SheetScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var sheedProvider = context.watch<SheetProvider>();
     if (controller.isAttached) {
-      sheedProvider.searchFocused ? openSheet() : hideSheet(sheedProvider);
+      sheedProvider.searchFocused
+          ? openSheet(sheedProvider)
+          : hideSheet(sheedProvider);
     }
     return DraggableScrollableSheet(
         controller: controller,
         minChildSize: 0.1,
         initialChildSize: 0.1,
-        maxChildSize: 0.8,
+        maxChildSize: 0.9,
         snap: true,
-        snapSizes: const [0.1, 0.6],
+        snapSizes: const [0.1, 0.6, 0.9],
         builder: (BuildContext context, ScrollController scrollController) {
           return Container(
               decoration: const BoxDecoration(
@@ -35,13 +39,13 @@ class SheetScreen extends StatelessWidget {
                   controller: scrollController,
                   child: Column(children: [
                     const SheetHeader(),
-                    buildBody(sheedProvider)
+                    buildBody(sheedProvider, scrollController)
                   ])));
         });
   }
 
-  void openSheet() {
-    controller.animateTo(0.8,
+  void openSheet(SheetProvider provider) {
+    controller.animateTo(provider.searchText.isEmpty ? 0.8 : 0.95,
         duration: const Duration(milliseconds: 200), curve: Curves.linear);
   }
 
@@ -52,13 +56,38 @@ class SheetScreen extends StatelessWidget {
     }
   }
 
-  Widget buildBody(SheetProvider provider) {
-    return Column(children: [
-      const SizedBox(height: 16),
-      if (provider.searchText.isEmpty)
-        const PresetsContainer()
-      else
-        const Text('123')
+  Widget buildBody(SheetProvider provider, ScrollController scrollController) {
+    return Padding(
+        padding: const EdgeInsets.only(left: 24, right: 24),
+        child: Column(children: [
+          const SizedBox(height: 16),
+          if (provider.searchText.isEmpty)
+            const PresetsContainer()
+          else
+            const Column(children: [SearchRow(), SearchRow(), SearchRow()])
+        ]));
+  }
+}
+
+class SearchRow extends StatelessWidget {
+  const SearchRow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      const SizedBox(width: 3),
+      Assets.search
+          .image(width: 24, height: 24, color: const Color(0xFF7C7C7B)),
+      const SizedBox(width: 11),
+      const Flexible(
+          child: Padding(
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              child: Text(
+                  'ГМИИ имени Пушкина, Галерея искусства стран Европы и Америки XIX – XX веков',
+                  style: TextStyle(
+                      fontFamily: FontFamily.deeDee,
+                      fontSize: 14,
+                      color: Color(0xFF121212)))))
     ]);
   }
 }
