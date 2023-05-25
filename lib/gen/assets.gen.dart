753de08a807937c202a41be576b88c1f4c3150be
Albert <alhafram@gmail.com>
2023-05-25 07:09:31 +0300

    Regenerate assets

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -17,9 +17,6 @@ class Assets {
   static const AssetGenImage backButton =
       AssetGenImage('assets/back_button.png');
   static const AssetGenImage bookmark = AssetGenImage('assets/bookmark.png');
-  static const AssetGenImage chat = AssetGenImage('assets/chat.png');
-  static const AssetGenImage chevronLeft =
-      AssetGenImage('assets/chevron-left.png');
   static const AssetGenImage childrenArtSchools =
       AssetGenImage('assets/children_art_schools.png');
   static const AssetGenImage comment = AssetGenImage('assets/comment.png');
@@ -59,8 +56,6 @@ class Assets {
         artSchools,
         backButton,
         bookmark,
-        chat,
-        chevronLeft,
         childrenArtSchools,
         comment,
         courses,
